3aae542524f1cc23882850e5cea7a00079488bdf
haruki <kkhk0869hs33@gmail.com>
2025-08-18 06:56:37 +0000

    ローカルでもコメントアウトせずに開発可能に変更

--- api/index.js
+++ api/index.js
@@ -136,8 +136,10 @@ app.get("/transactions/summary", async (req, res) => {
   }
 });
 
-// app.listen(process.env.PORT, () => {
-//   console.log(`Server running on port ${process.env.PORT}`);
-// });
+if (process.env.NODE_ENV !== "production") {
+  app.listen(process.env.PORT, () => {
+    console.log(`Server running on port ${process.env.PORT}`);
+  });
+}
 
 export default app;
